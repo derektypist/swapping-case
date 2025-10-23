3f99a4ba4be2e77913995edf85554b5ce34715f8
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2025-10-23 13:26:26 +0000

    Added keyup and click events at script.js

--- js/script.js
+++ js/script.js
@@ -23,3 +23,20 @@ function swapCase(str) {
 
     return updatedStr;
 }
+
+// Apply keyup event for id of mytext
+$('#mytext').on('keyup', function () {
+    $('#results').html(``);
+    const str = $('#mytext').val();
+    if (str.length == 0) {
+        $('#results').html(``);
+    } else {
+        $('#results').append(`Original String is ${str} <br>`);
+        $('#results').append(`String after swapping cases is ${swapCase(str)} <br>`);
+    }
+});
+
+// Apply click event for id of clearBtn
+$('#clearBtn').on('click', function () {
+    $('#results').html(``);
+});
